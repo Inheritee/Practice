b88b00f406a18e0e44c62f559b75b28b24fbce7b
Inheritee <royheyb13@gmail.com>
2022-07-10 20:19:23 +0200

    update

--- js/login/script.js
+++ js/login/script.js
@@ -19,6 +19,14 @@ var database = [
     {
         username: "john",
         password: "secret"
+    },
+    {
+        username: "Sally",
+        password: "123"
+    },
+    {
+        username: "ingrid",
+        password: "777"
     }
 ];
 
@@ -30,19 +38,32 @@ var newsFeed = [
     {
         username: "Eline",
         timeline: "day 3 of learning"
+    },
+    {
+        username: "Mitchel",
+        timeline: "JS is VERY cool"
     }
 ];
 
+function isUserValid(username, password) {
+    for (var i=0; i < database.length; i++) {
+        if (database[i].username === username &&
+            database[i].password === password) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function signIn(username, password){
+    if (isUserValid(username, password)) {
+        console.log(newsFeed);
+    } else {
+        alert("sorry, wrong username and password");
+    }
+}
+
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("whats your password?");
-
-function signIn(user, pass) {
-    if (user === database[0].username && 
-        pass === database[0].password) {
-        console.log(newsFeed);
-        } else {
-            alert("Sorry, wrong username and password!");
-        }
-}
 
 signIn(userNamePrompt, passwordPrompt);
